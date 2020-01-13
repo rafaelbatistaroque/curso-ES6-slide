98884420ef5fea1055a3a0a177fcf8ce3ec201f9
Rafael Batista <maranata_rafa@hotmail.com>
2020-01-13 14:53:39 -0400

    Cálculo de distâncias quando ocorrem eventos.

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -2,16 +2,31 @@ export default class Slide {
     constructor(slide, wrapper) {
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
+        this.distancia = { posicaoFinal: 0, inicioEixoX: 0, totalMovimento: 0 };
     }
     removerPadroes(event) {
         event.preventDefault();
+        this.distancia.inicioEixoX = event.clientX;
+        console.log(this.distancia.inicioEixoX);
+
         this.wrapper.addEventListener('mousemove', this.iniciarEventoAoMoverMouseSobreImagem);
     }
+    moverSlide(distanciaX) {
+        this.distancia.ultimaPosicao = distanciaX;
+        this.slide.style.transform = `translate3d(${distanciaX}px, 0, 0)`;
+    }
+    atualizarPosicaoCliqueMouse(clientX) {
+        this.distancia.totalMovimento = (this.distancia.inicioEixoX - clientX) * 1.6;
+        return this.distancia.posicaoFinal - this.distancia.totalMovimento;
+
+    }
     iniciarEventoAoMoverMouseSobreImagem() {
-        console.log('moveu');
+        const posicaoFinal = this.atualizarPosicaoCliqueMouse(event.clientX);
+        this.moverSlide(posicaoFinal);
     }
     encerrarEventoAoTirarMouseSobreImagem() {
         this.wrapper.removeEventListener('mousemove', this.iniciarEventoAoMoverMouseSobreImagem);
+        this.distancia.posicaoFinal = this.distancia.ultimaPosicao;
     }
     bindEventos() {
         this.removerPadroes = this.removerPadroes.bind(this);
